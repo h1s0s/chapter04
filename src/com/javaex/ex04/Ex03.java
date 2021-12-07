107b10bda2186e61ca173a6a948899dee6c59710
한상선 <hs94122@naver.com>
2021-12-07 15:59:17 +0900

    2021-12-07 16:00

--- src/com/javaex/ex04/Ex03.java
+++ src/com/javaex/ex04/Ex03.java
@@ -6,16 +6,17 @@ import java.util.Set;
 public class Ex03 {
 
 	public static void main(String[] args) {
-		//로또 만들기
+		//로또 만들기-내가짠 코드
 		Set<Integer> lSet = new HashSet<Integer>();
 		
 		while(true){
-			if(lSet.size()!=6) {
-				lSet.add((int)(Math.random()*45)+1);	
-			} else {
-				System.out.println(lSet);
-				break;
+			if(lSet.size()==6) {
+				break;	
+			} 
+			lSet.add((int)(Math.random()*45)+1);
 			}
+		for(int no:lSet) {
+			System.out.print(no+"\t");
 		}
-	}
+	}	
 }
---
+++ src/com/javaex/ex04/Ex031.java
@@ -0,0 +1,23 @@
+package com.javaex.ex04;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Ex031 {
+
+	public static void main(String[] args) {
+		// 로또만들기-강사님 코드
+		Set<Integer> iSet = new HashSet<Integer>();
+		while(true) {
+			if(iSet.size() >=6) {
+				break;
+			} 
+			int num = (int)(Math.random()*45)+1;
+			iSet.add(num);
+		}
+		for(int no:iSet) {
+			System.out.print(no+"\t");
+		}
+	}
+
+}
